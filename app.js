6917329300eb964f3f0fdbeab5177c552efe0f2a
qmxz79 <150225049+qmxz79@users.noreply.github.com>
2024-07-09 03:15:45 +0800

    Add files via upload
    first upload

---
+++ app.js
@@ -0,0 +1,40 @@
+
+#### 3. `app.js`
+
+```javascript
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync('logs', logs);
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log('登录成功，code:', res.code);
+      }
+    });
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              this.globalData.userInfo = res.userInfo;
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res);
+              }
+            }
+          });
+        }
+      }
+    });
+  },
+  globalData: {
+    userInfo: null
+  }
+});
